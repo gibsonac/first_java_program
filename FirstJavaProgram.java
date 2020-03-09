b41d4cc7cacfcfaa33e08aa61e9b2d37d5757129
Adam Gibson <gibsoncadam@gmail.com>
2020-03-09 11:48:02 -0700

    learning Java

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Adam");
+        System.out.println("I am 30 years old");
+        System.out.println("My Hometown is Stafford, VA");
+    }
+}
